f3e46935d860ad7e73666b477f3ecec3ef6f17fd
ValentinoTriadi <valent122016@gmail.com>
2023-10-31 15:44:04 +0700

    Inisiasi template

---
+++ Backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+@app.get("/")
+async def main():
+    return "hALO"
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Algeo02-22134
+
+FIRST RUN:
+1. Create virtual environment
+
+RUN FASTAPI:
+1. cd Backend
+2. type "uvicorn main:app"
+
+RUN REACT:
+1. cd Frontend/tubes-fullstack
+2. type "npm install"
+3. type "npm run start"
