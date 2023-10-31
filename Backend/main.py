2576ca62980b6f510e456341abae3dc719dd5d27
ValentinoTriadi <valent122016@gmail.com>
2023-10-31 20:30:37 +0700

    feat(uploadimg): fetch image from react to fastapi

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,12 @@
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, File, Form
 from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import io
+import os
 
 app = FastAPI()
+
+# origins = ["http://localhost:3000/"]    
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,6 +16,37 @@ app.add_middleware(
     allow_credentials=True,
 )
 
+dir_path = 'Backend\dataset'
+def countFile():
+    count = 0
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)):
+            count += 1
+    return count
+
+namaFile = []
+
+
+
+@app.get("/u/")
+async def asdfsad():
+    return "blabsss"
 @app.get("/")
-async def main():
+async def bro():
     return "hALO"
+
+@app.post("/upload/")
+# async def receiveFile(file: bytes = File(...), namafile: str = Form(...)):
+async def receiveFile(file: bytes = File(...)):
+
+    # print(namafile)
+    image = Image.open(io.BytesIO(file))
+    if (os.path.exists("Backend\dataset")):
+        print("TRUE")
+    else:
+        print("FALSE")
+        os.mkdir("Backend\dataset")
+    i = countFile()
+    image = image.save(f"Backend\dataset\img{i}.jpg")
+
+    return {"uploadStatus": "Complete"}
--- Frontend/tubes-fullstack/src/App.js
+++ Frontend/tubes-fullstack/src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+
+
 
 function App() {
+  
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const filechangehandler = (e) => {
+    setSelectedFile(e.target.files[0]);
+    console.log(e.target.files[0]);
+  }
+
+  const handlesubmit = (e) => {
+    const formData = new FormData();
+    formData.append(
+      "file",
+      selectedFile,
+      selectedFile.name
+    );
+    // formData.append(
+    //   "namafile",
+    //   selectedFile.target.files[0].name,
+    // )
+
+    const requestOptions = {
+      method: "POST",
+      body: formData,
+    }
+
+    fetch("http://127.0.0.1:8000/upload/", requestOptions)
+    .then(response => response.json())
+    .then(function(response){
+      console.log(response);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <fieldset>
+          <input onChange={filechangehandler} name="image" type="file" accept=".jpg, .jpeg, .png"></input>
+        </fieldset>
+        <button onClick={handlesubmit}>Upload</button>
+      </form>
     </div>
   );
 }
