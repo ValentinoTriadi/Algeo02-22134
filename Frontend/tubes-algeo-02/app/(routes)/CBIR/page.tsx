59345345015d3f00d86125b26dbbf9f569b896cf
AtqiyaHaydar <ashford1524@gmail.com>
2023-11-01 22:58:18 +0700

    Frontend Ver 4

--- Frontend/tubes-algeo-02/app/(routes)/CBIR/page.tsx
+++ Frontend/tubes-algeo-02/app/(routes)/CBIR/page.tsx
@@ -14,21 +14,32 @@ const CBIR: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [namaFile, setNamaFile] = useState<string | null>(null);
 
+  // Ref button ke input
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const folderInputRef = useRef<HTMLInputElement | null>(null);
 
+  // Untuk input file
   const handleButtonClick = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click();
     }
   }
 
+  // Untuk input dataset
+  const handleButtonClick2 = () => {
+    if (folderInputRef.current) {
+      folderInputRef.current.click();
+    }
+  }
+
+  // Fungsi mengganti nama dan tampilan file
   const inputImageHandler = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const file = e.target.files[0];
       if (file) {
-        setImageName(file.name);
+        setImageName(file.name.substring(0, 40));
         const imgUrl = URL.createObjectURL(file);
-        setImageUrl(imgUrl); // Hiraukan ini
+        setImageUrl(imgUrl); // Hiraukan ini kalau merah
       }
     }
   }
@@ -41,42 +52,43 @@ const CBIR: React.FC = () => {
     return;
   }
 
-  const fileChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const file = e.target.files[0];
-      setSelectedFile(file);
-      setNamaFile(file.name);
-      console.log(file);
-    }
-  }
+  // Fungsi upload dataset
+  // const fileChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.files && e.target.files.length > 0) {
+  //     const file = e.target.files[0];
+  //     setSelectedFile(file);
+  //     setNamaFile(file.name);
+  //     console.log(file);
+  //   }
+  // }
   
-  const uploadDatasetHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    console.log(namaFile);
+  // const uploadDatasetHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   console.log(namaFile);
 
-    if (!selectedFile) {
-      console.error("No file selected");
-      return;
-    }
+  //   if (!selectedFile) {
+  //     console.error("No file selected");
+  //     return;
+  //   }
 
-    const formData = new FormData();
-    formData.append("file", selectedFile, selectedFile.name);
-    formData.append("namafile", namaFile);
+  //   const formData = new FormData();
+  //   formData.append("file", selectedFile, selectedFile.name);
+  //   formData.append("namafile", namaFile);
 
-    const requestOptions: RequestInit = {
-      method: "POST",
-      body: formData,
-    }
+  //   const requestOptions: RequestInit = {
+  //     method: "POST",
+  //     body: formData,
+  //   }
 
-    fetch("http://127.0.0.1:8000/upload/", requestOptions)
-    .then(response => response.json())
-    .then(function(response){
-      console.log(response);
-    })
-    .catch(error => {
-      console.error("Error:", error);
-    });
-  }
+  //   fetch("http://127.0.0.1:8000/upload/", requestOptions)
+  //   .then(response => response.json())
+  //   .then(function(response){
+  //     console.log(response);
+  //   })
+  //   .catch(error => {
+  //     console.error("Error:", error);
+  //   });
+  // }
 
   return (
     <section className='w-full flex flex-col items-center py-8 justify-center'>
@@ -86,7 +98,12 @@ const CBIR: React.FC = () => {
 
         {/* IMAGE */}
         <div className="border-2 border-black rounded-md h-400 w-400object-fill">
-          <Image alt="Gambar masukan" src={imageUrl} height={400} width={400}/>
+          <Image 
+            alt="Gambar masukan" 
+            src={imageUrl} 
+            height={400} 
+            width={400}
+          />
         </div>
         <div className="flex flex-col justify-between md:px-4 md:mt-0 gap-y-12 md:gap-y-0">
 
@@ -109,23 +126,30 @@ const CBIR: React.FC = () => {
                 ref={fileInputRef}  
               />
             </div>
-            
             <p>{imageName}</p>
           </div>
 
+          {/* SEARCH IMAGE */}
           <div className="flex flex-col gap-y-2">
             <div className="flex gap-x-2">
               <p>Color</p>
-              <Switch onClick={switchHandler}/>
+              <Switch 
+                onClick={switchHandler}
+              />
               <p>Texture</p>
             </div>
-            <Button size='lg' className="border-2 border-white hover:bg-white hover:text-black hover:border-2 hover:border-black" onClick={searchHandler}>
+            <Button 
+              size='lg' 
+              className="border-2 border-white hover:bg-white hover:text-black hover:border-2 hover:border-black" 
+              onClick={searchHandler}
+            >
               Search
             </Button>
           </div>
         </div>
       </div>
 
+      {/* RESULT */}
       <div className="w-[400px] md:w-[750px] mt-[75px] border-t border-1 border-black py-4">
         <div className="flex justify-between">
           <p>Result</p>
@@ -134,16 +158,28 @@ const CBIR: React.FC = () => {
         {/* PAGINATION */}
       </div>
 
+      {/* UPLOAD DATASET */}
       <div className="w-[400px] md:w-[750px] mt-[75px] border-t border-1 border-black py-4 flex flex-col items-center gap-y-4">
-        <p className="font-semibold">Upload Dataset</p>
-        <Input 
-          type='file' 
-          id='picture' 
-          className="max-w-[300px] rounded-full border-2 border-black bg-white hover:bg-white hover:text-black hover:border-2 hover:border-black" 
-          onChange={uploadDatasetHandler} />
+        <p className="text-xl">Upload Dataset</p>
+        <Button 
+          size='lg'
+          onClick={handleButtonClick2} 
+          className="border-2 rounded-full border-white hover:bg-white hover:text-black hover:border-2 hover:border-black">
+          <p className="font-semibold">Upload</p>
+        </Button>
+        <div className="hidden">
+          <Input 
+            type='file' 
+            id='picture' 
+            className="max-w-[300px] rounded-full border-2 border-black bg-white hover:bg-white hover:text-black hover:border-2 hover:border-black" 
+            // onChange={fileChangeHandler} 
+            ref={folderInputRef}
+          />
+        </div>
       </div>
     </section>
   )
 }
 
 export default CBIR
+
