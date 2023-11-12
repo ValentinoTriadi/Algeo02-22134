12ed6ae9bee055e86fafc3a08c7ad7c7c82c617d
ValentinoTriadi <valent122016@gmail.com>
2023-11-12 22:10:00 +0700

    feat(CBIRTekstur): Update and clean up coment

--- Backend/CBIRTekstur.py
+++ Backend/CBIRTekstur.py
@@ -23,36 +23,20 @@ def convertToGrayscale(imageArray):
     return new
 
 def quantization(grayscale_matrix, num_levels=256):
-    quantization_range = np.linspace(0, 255, num_levels)
-    quantized_matrix = np.digitize(grayscale_matrix, quantization_range) - 1
     # metode bilinear interpolation supaya jadi 256x256
-    # resized_matrix = np.array(Image.fromarray(quantized_matrix.astype('int')).resize((256, 256), Image.BILINEAR))
-    # return resized_matrix
+    resized_matrix = np.array(Image.fromarray(grayscale_matrix.astype('int')).resize((256, 256), Image.BILINEAR))
+    quantization_range = np.linspace(0, 255, num_levels)
+    quantized_matrix = np.digitize(resized_matrix, quantization_range) - 1
     return quantized_matrix
-
-# def create_co_occurrence_matrix(input_matrix, quantization_level):
-    # Membuat framework matrix
-    # framework_matrix = np.zeros((quantization_level, quantization_level), dtype=int)
-
-    # # Mengambil pasangan pixel secara horizontal
-    # x = input_matrix[:, :-1].flatten()
-    # y = input_matrix[:, 1:].flatten()
-
-    # # Increment element di framework_matrix[x][y]
-    # np.add.at(framework_matrix, (x, y), 1)
-
-    # # Membuat matriks simetris
-    # return np.add(framework_matrix, np.transpose(framework_matrix))
+    # return resized_matrix
 
 def create_co_occurrence_matrix(input_matrix, quantization_level):
     framework_matrix = np.zeros((quantization_level, quantization_level), dtype=int)
-
-    np.add.at(framework_matrix, (input_matrix[:, :-1], input_matrix[:, 1:]), 1)
-
+    input = input_matrix.astype("int")
+    np.add.at(framework_matrix, (input[:, :-1], input[:, 1:]), 1)
     glcm_matrix = np.add(framework_matrix, np.transpose(framework_matrix))
 
     return glcm_matrix
-
 
 def calculate_contrast_from_glcm(glcm_matrix):
     i, j = np.indices(glcm_matrix.shape)
@@ -65,30 +49,6 @@ def calculate_homogeneity_from_glcm(glcm_matrix):
 def calculate_entropy_from_glcm(glcm_matrix):
     return -np.sum(glcm_matrix * np.log(glcm_matrix + np.spacing(1)))
 
-# def calculate_contrast_from_glcm(glcm_matrix):
-#     contrast = 0
-#     row, col = glcm_matrix.shape
-#     for i in range(row):
-#         for j in range(col):
-#             contrast += glcm_matrix[i, j] * (i - j)**2
-#     return contrast
-
-# def calculate_homogeneity_from_glcm(glcm_matrix):
-#     homogeneity = 0
-#     row, col = glcm_matrix.shape
-#     for i in range(row):
-#         for j in range(col):
-#             homogeneity += glcm_matrix[i, j] / (1 + (i - j)**2)
-#     return homogeneity
-
-# def calculate_entropy_from_glcm(glcm_matrix):
-#     entropy = 0
-#     row, col = glcm_matrix.shape
-#     for i in range(row):
-#         for j in range(col):
-#             entropy -= glcm_matrix[i, j] * np.log(glcm_matrix[i, j] + np.spacing(1))
-#     return entropy
-
 def create_texture_vector(glcm_matrix):
     contrast = calculate_contrast_from_glcm(glcm_matrix)
     homogeneity = calculate_homogeneity_from_glcm(glcm_matrix)
@@ -98,21 +58,13 @@ def create_texture_vector(glcm_matrix):
 
 def pictureToTextureVector(img, isUpload, filename):
     rgb = np.array(img)
-    # print("RGB",rgb)
     grayscale_matrix = convertToGrayscale(rgb)
-    # print("G",grayscale_matrix)
-    quantized_matrix = quantization(grayscale_matrix)
-    # print("q",quantized_matrix)
-    glcm_matrix = create_co_occurrence_matrix(quantized_matrix, 256)
-    print("GLCM",glcm_matrix)
-    sum_glcm_matrix = np.sum(glcm_matrix)
-    norm_glcm_matrix = glcm_matrix[...] / sum_glcm_matrix 
-    # print("NORM",norm_glcm_matrix)
+    glcm_matrix = create_co_occurrence_matrix(grayscale_matrix, 256)
+    norm_glcm_matrix = glcm_matrix / np.sum(glcm_matrix)
     texture_vector = create_texture_vector(norm_glcm_matrix)
     if (isUpload):
         with open("./cache.txt", "a") as f:
             f.write(filename + "|[" + str(texture_vector[0]) + "," + str(texture_vector[1]) + "," +str(texture_vector[2]) + "]\n")
-
     return texture_vector
 
 def cosine_similarity(a, b):
@@ -126,7 +78,6 @@ def cosine_similarity(a, b):
 def parseTXT(Line):
     r = Line.rstrip().split("|")
     r[1] = ast.literal_eval(r[1])
-
     return r
 
 def compareTekstur(filename):
